1f8c1f8826c22cc82d83adc722a59f838340e400
bgora <girsch@ventx.de>
2018-02-20 02:27:19 +0000

    added --agree-tos to certbot

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,5 +7,5 @@ envsubst '\$DOMAIN' < /nginx.conf.template > /etc/nginx/sites-available/default
 nginx 
 #/usr/bin/certbot certonly --standalone --preferred-challenges http -d ${domain} --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/
 mkdir -p /var/jenkins_home/certs/www
-certbot --authenticator webroot --installer nginx --webroot -w /var/jenkins_home/certs/www -d $DOMAIN -m martin@ventx.de --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/ -n
+certbot --authenticator webroot --installer nginx --webroot -w /var/jenkins_home/certs/www -d $DOMAIN -m martin@ventx.de --agree-tos --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/ -n
 /sbin/tini -- /usr/local/bin/jenkins.sh
