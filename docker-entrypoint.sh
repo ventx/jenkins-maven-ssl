ecf03b9d02b938f74b11dbf9c540582def15a734
bgora <girsch@ventx.de>
2018-02-20 12:52:45 +0000

    changed order in docker-entrypoint.sh + letsencrypt runs standalone

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#get cert move to jenkins start jenkins
+#get cert 
+letsencrypt certonly --standalone -d ${DOMAIN} -n -m martin@ventx.de --agree-tos --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/
+# convert template to nginx conf with envsubst
 # envsubst takes a whitespace sepratet list of vars to replace. Otherwise it will break things like $http_host
 # example: envsubst '\$DOMAIN \$PORT' < /nginx.config.template > /etc/nginx/sites-available/default
 # will replace $DOMAIN and $PORT only
 envsubst '\$DOMAIN' < /nginx.conf.template > /etc/nginx/sites-available/default
+# start nginx
 nginx 
 #/usr/bin/certbot certonly --standalone --preferred-challenges http -d ${domain} --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/
-mkdir -p /var/jenkins_home/certs/www
-certbot --authenticator webroot --installer nginx --webroot -w /var/jenkins_home/certs/www -d $DOMAIN -m martin@ventx.de --agree-tos --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/ -n
+#mkdir -p /var/jenkins_home/certs/www
+#certbot --authenticator webroot --installer nginx --webroot -w /var/jenkins_home/certs/www -d $DOMAIN -m martin@ventx.de --agree-tos --config-dir /var/jenkins_home/certs/config --cert-path /var/jenkins_home/certs/ -n
+# start jenkins
 /sbin/tini -- /usr/local/bin/jenkins.sh
